ebcaf662573521c9ee649fb06528a72e848a577f
zhangling.me@foxmail.com <zhangling.me@foxmail.com>
2022-01-05 01:46:27 +0800

    feat: 跑通自动点餐流程

--- main.js
+++ main.js
@@ -1,26 +1,12 @@
 const schedule = require('./lib/schedule')
-const meicanApi = require('./lib/meicanApi')
-const config = require('./config')
+const autoOrder = require('./src/autoOrder')
 
 /**
  * 入口文件（主逻辑）
  */
-;(async function main() {
-  // todo: 改为循环用户，且随机等待1-5秒
-  const { username, password } = config.users[0]
-	// 登录，拿到 remember = token
-	const { remember } = await meicanApi.login(username, password)
-	// 请求当前点餐状态
-	const { restaurantItemList } = await meicanApi.ordersShow(remember)
-	if (restaurantItemList?.length === 0) {
-		// todo: 拉取商家和商品列表
-		// todo: 下单
-	} else {
-		console.log("用户XXX已点餐，自动点餐失败！")
-	}
-})()
-
+// test: 立即执行一次
+// autoOrder()
 // 每天0点10分 8点 跑一次自动点餐脚本
 const crons = ['0 10 0 * * *', '0 0 8 * * *']
 // const crons = ['* * * * * *']
-crons.forEach(cron => schedule(cron, meicanApi.login))
+crons.forEach(cron => schedule(cron, autoOrder))
---
+++ src/autoOrder.js
@@ -0,0 +1,37 @@
+const meicanApi = require('../lib/meicanApi')
+const config = require('../config')
+
+/**
+ * 自动点餐
+ */
+module.exports = function() {
+  ;(async function main() {
+    // todo: 改为循环用户，且随机等待1-5秒，抽离批量方法
+    const { username, password } = config.users[0]
+    // 登录，拿到 remember = token
+    const { remember } = await meicanApi.login(username, password)
+    // 请求当前点餐结果
+    const { dateList } = await meicanApi.ordersShow(remember)
+    const tabUniqueId = dateList[0].calendarItemList[0].userTab.uniqueId
+    const isOrdered = !!dateList[0].calendarItemList[0].corpOrderUser
+    if (isOrdered) {
+      console.log(`用户 ${username} 已点餐，自动点餐失败！`)
+      return
+    }
+    // 拉取餐厅列表
+    const { restaurantList } = await meicanApi.restaurantsList(remember, tabUniqueId)
+    // 获取餐品列表
+    // todo: 循环 restaurantList，promise.all请求，拼凑结果
+    const { dishList } = await meicanApi.restaurantsShow(remember, tabUniqueId, restaurantList[0].uniqueId)
+    // 下单
+    // todo: dishList从1开始，0为标题，下单自定义，抽离
+    try {
+      const orderResponseData = await meicanApi.orderAdd(remember, tabUniqueId, dishList[1].id)
+      if (orderResponseData.status === 'SUCCESSFUL') {
+        console.log(`用户 ${username} 自动点餐成功，餐品为 ${dishList[1].name}`)
+      }
+    } catch (error) {
+      console.log(`用户 ${username} 已点餐，自动点餐失败！`)
+    }
+  })()
+}
