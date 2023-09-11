7eef104a74c25e3d085b2efc720760201fd00e26
lingz <zhangling.me@foxmail.com>
2023-09-12 01:24:41 +0800

    fix

--- src/autoOrder.js
+++ src/autoOrder.js
@@ -7,10 +7,13 @@ const config = require('../config')
 module.exports = function({username, password, defaultOrderType, orderType}) {
   ;(async function main() {
     /* step1: 登录，拿到 remember = token */
-    const { remember } = await meicanApi.login(username, password)
+    const { remember, access_token } = await meicanApi.login(username, password)
+    console.log('remember: ', remember)
+    console.log('access_token: ', access_token)
     /* step2: 请求当前点餐结果 */
     // todo: 由于总是返回前一已点餐品，所以终止检查逻辑
-    const { dateList } = await meicanApi.ordersShow(remember)
+    const { dateList } = await meicanApi.ordersShow(remember, access_token)
+
     const tabUniqueId = dateList[0].calendarItemList[0].userTab.uniqueId
     // const isOrdered = !!dateList[0].calendarItemList[0].corpOrderUser
     // if (isOrdered) {
@@ -18,12 +21,12 @@ module.exports = function({username, password, defaultOrderType, orderType}) {
     //   return
     // }
     /* step3: 拉取餐厅列表 */
-    const { restaurantList } = await meicanApi.restaurantsList(remember, tabUniqueId)
+    const { restaurantList } = await meicanApi.restaurantsList(remember, tabUniqueId, access_token)
     // console.log("餐厅列表拉取成功！", restaurantList)
     /* step4: 获取餐品列表 */
     let dishLists = [];
     for (const item of restaurantList) {
-      const { dishList } = await meicanApi.restaurantsShow(remember, tabUniqueId, item.uniqueId)
+      const { dishList } = await meicanApi.restaurantsShow(remember, tabUniqueId, item.uniqueId, access_token)
       // dishList餐品从1开始。0为标题，不可点餐，需要去除。
       dishList.shift();
       // console.log("餐品列表拉取成功！", dishList)
@@ -48,7 +51,7 @@ module.exports = function({username, password, defaultOrderType, orderType}) {
     }
     console.log('>>>>>>>>', dishId)
     try {
-      const orderResponseData = await meicanApi.orderAdd(remember, tabUniqueId, dishId)
+      const orderResponseData = await meicanApi.orderAdd(remember, tabUniqueId, dishId, access_token)
       if (orderResponseData.status === 'SUCCESSFUL') {
         console.log(`用户 ${username} 自动点餐成功，餐品为 ${dishLists[randomIndex].name}`)
       }
